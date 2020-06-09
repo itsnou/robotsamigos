21a73167d641b6c20c65777445faea4e6907b595
itsnou <emiliano.meneses@gmail.com>
2020-06-09 08:06:01 -0300

    Updates

---
+++ precache-manifest.07271596bedd771b34d29278a81b84f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e793f8b7afba3de8b99a92292dce539",
+    "url": "/robotsamigos/index.html"
+  },
+  {
+    "revision": "d6710cc6aec863323f81",
+    "url": "/robotsamigos/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4528bb2424134e503c79",
+    "url": "/robotsamigos/static/css/main.51dda8d6.chunk.css"
+  },
+  {
+    "revision": "d6710cc6aec863323f81",
+    "url": "/robotsamigos/static/js/2.daa72ce5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotsamigos/static/js/2.daa72ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4528bb2424134e503c79",
+    "url": "/robotsamigos/static/js/main.27440bcc.chunk.js"
+  },
+  {
+    "revision": "e499de0ab4e985c678df",
+    "url": "/robotsamigos/static/js/runtime-main.7095a39a.js"
+  },
+  {
+    "revision": "d995a43750259d4b6fe81579f8aed3d6",
+    "url": "/robotsamigos/static/media/Nerik-normal.d995a437.woff"
+  }
+]);
